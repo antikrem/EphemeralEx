0f1409bfe6308a8c53ef6158ad70da0c5d45a87a
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-10-25 00:33:51 +1000

    Added Some.String

--- EphemeralEx/Tests/Some.cs
+++ EphemeralEx/Tests/Some.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+
+using EphemeralEx.Extensions;
+
 
 namespace EphemeralEx.Tests
 {
@@ -8,5 +12,11 @@ namespace EphemeralEx.Tests
 
         public static int Int(int bottom = 0, int top = int.MaxValue)
             => _random.Next(bottom, top);
+
+        public static string String(int length = 16)
+            => Enumerable.Range(0, length)
+                .Select(_ => _random.Next(0, 26))
+                .Select(offset => (char)('a' + offset))
+                .Concat();
     }
 }
