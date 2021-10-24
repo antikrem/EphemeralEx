64ce91092a66d6759300620e4b722769341b2858
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-10-24 23:45:29 +1000

    Added Some

---
+++ EphemeralEx/Tests/Some.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EphemeralEx.Tests
+{
+    public static class Some
+    {
+        static readonly Random _random =  new();
+
+        public static int Int(int bottom = 0, int top = int.MaxValue)
+            => _random.Next(bottom, top);
+    }
+}
