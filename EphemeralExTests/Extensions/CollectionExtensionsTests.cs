5b344d67d9b883907fff2b2200deb898effd8516
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-10-28 00:27:46 +1000

    Added tests

--- EphemeralExTests/Extensions/CollectionExtensionsTests.cs
+++ EphemeralExTests/Extensions/CollectionExtensionsTests.cs
@@ -6,7 +6,8 @@ using NUnit.Framework;
 using FluentAssertions;
 
 using EphemeralEx.Tests;
-
+using System.Collections.Generic;
+using System;
 
 namespace EphemeralExTests.Extensions
 {
@@ -88,6 +89,103 @@ namespace EphemeralExTests.Extensions
             var result = start.ChainCall(sequence, (str, element) => str + element.ToString());
 
             result.Should().Be("Numbers:" + item1.ToString() + item2.ToString() + item3.ToString());
+        }
+
+        [Test]
+        public void AddAndReturn_WithNewEntry_AddsToDictionary()
+        {
+            var dictionary = new Dictionary<int, string>() { {1, "foo"} };
+
+            var result = dictionary.AddAndReturn(2, "bar");
+
+            result.Should().Be("bar");
+            dictionary.Should().HaveCount(2);
+            dictionary.Should().BeEquivalentTo(
+                    new Dictionary<int, string>()
+                    {
+                        { 1, "foo" },
+                        { 2, "bar" }
+                    }
+                );
+        }
+
+        [Test]
+        public void AddAndReturn_WithExistingEntry_ShouldThrow()
+        {
+            var dictionary = new Dictionary<int, string>() { { 1, "foo" } };
+
+            dictionary.Invoking(d => d.AddAndReturn(1, "bar"))
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GetOrAdd_WithNewEntry_CreatesNewEntryAndReturns()
+        {
+            var dictionary = new Dictionary<int, string>() { { 1, "foo" } };
+
+            var result = dictionary.GetOrAdd(2, "bar");
+
+            result.Should().Be("bar");
+            dictionary.Should().HaveCount(2);
+            dictionary.Should().BeEquivalentTo(
+                    new Dictionary<int, string>()
+                    {
+                        { 1, "foo" },
+                        { 2, "bar" }
+                    }
+                );
+        }
+
+        [Test]
+        public void GetOrAdd_WithExistingEntry_ReturnsWithoutCreating()
+        {
+            var dictionary = new Dictionary<int, string>() { { 1, "foo" } };
+
+            var result = dictionary.GetOrAdd(1, "bar");
+
+            result.Should().Be("foo");
+            dictionary.Should().HaveCount(1);
+            dictionary.Should().BeEquivalentTo(
+                    new Dictionary<int, string>()
+                    {
+                        { 1, "foo" }
+                    }
+                );
+        }
+
+        [Test]
+        public void GetOrAddOverload_WithNewEntry_CreatesNewEntryAndReturns()
+        {
+            var dictionary = new Dictionary<int, string>() { { 1, "foo" } };
+
+            var result = dictionary.GetOrAdd(2, () => "bar");
+
+            result.Should().Be("bar");
+            dictionary.Should().HaveCount(2);
+            dictionary.Should().BeEquivalentTo(
+                    new Dictionary<int, string>()
+                    {
+                        { 1, "foo" },
+                        { 2, "bar" }
+                    }
+                );
+        }
+
+        [Test]
+        public void GetOrAddOverload_WithExistingEntry_ReturnsWithoutCreating()
+        {
+            var dictionary = new Dictionary<int, string>() { { 1, "foo" } };
+
+            var result = dictionary.GetOrAdd(1, () => "bar");
+
+            result.Should().Be("foo");
+            dictionary.Should().HaveCount(1);
+            dictionary.Should().BeEquivalentTo(
+                    new Dictionary<int, string>()
+                    {
+                        { 1, "foo" }
+                    }
+                );
         }
     }
 }
