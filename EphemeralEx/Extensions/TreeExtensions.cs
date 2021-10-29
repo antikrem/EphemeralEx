0bd6f90e12400143cd07c98b8f9da545e79007ed
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-10-30 01:54:30 +1000

    Added tree walking

---
+++ EphemeralEx/Extensions/TreeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EphemeralEx.Extensions
+{
+    public static class TreeExtensions
+    {
+        public static IEnumerable<T> TraverseTree<T>(this T element, Func<T, IEnumerable<T>> detree)
+        {
+            yield return element;
+
+            var subElements = detree(element)
+                .SelectMany(subParent => subParent.TraverseTree(detree));
+
+            foreach (var subElement in subElements)
+            {
+                yield return subElement;
+            }
+        }
+    }
+}
---
+++ EphemeralExTests/Extensions/TreeExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using EphemeralEx.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EphemeralExTests.Extensions
+{
+    class TreeExtensionsTests
+    {
+        [Test]
+        public void TraverseTree_WithTreeStructure_TraverseTree()
+        {
+            var tree = new Node(
+                0,
+                new List<Node>() {
+                    new Node(1, Enumerable.Empty<Node>().ToList()),
+                    new Node(
+                        2,
+                        new List<Node>() {
+                            new Node(3, Enumerable.Empty<Node>().ToList()),
+                            new Node(4, Enumerable.Empty<Node>().ToList())
+                        }
+                    )
+                }
+            );
+
+            var results = tree.TraverseTree(node => node.Nodes);
+
+            results
+                .Select(node => node.Value)
+                .Should()
+                .BeEquivalentTo(new List<int> { 0, 1, 2, 3, 4 });
+        }
+
+        private record Node(int Value, List<Node> Nodes);
+    }
+}
