6e6ae1f9a2669f54448282c7b3cb631303ebb444
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-10-30 19:52:04 +1000

    Specified dictionary key type as not nullable

--- EphemeralEx/Extensions/DictionaryExtensions.cs
+++ EphemeralEx/Extensions/DictionaryExtensions.cs
@@ -7,17 +7,20 @@ namespace EphemeralEx.Extensions
     public static class DictionaryExtensions
     {
         public static TValue AddAndReturn<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue backup)
+            where TKey : notnull
         {
             dictionary.Add(key, backup);
             return backup;
         }
 
-        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue alternative) 
-            => dictionary.TryGetValue(key, out TValue? val) 
-                ? val 
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue alternative)
+            where TKey : notnull
+            => dictionary.TryGetValue(key, out TValue? val)
+                ? val
                 : dictionary.AddAndReturn(key, alternative);
 
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> generator)
+            where TKey : notnull
             => dictionary.TryGetValue(key, out TValue? val)
                 ? val
                 : dictionary.AddAndReturn(key, generator());
