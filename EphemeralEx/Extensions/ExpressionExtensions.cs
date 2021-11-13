dd2e7ea6ffb271613740daa58c88973e69cdebe2
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-11-14 00:34:12 +1000

    Added inline actor

---
+++ EphemeralEx/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace EphemeralEx.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        static public T Do<T>(this T obj, Action<T> action)
+        {
+            action(obj);
+            return obj;
+        }
+    }
+}
---
+++ EphemeralExTests/Extensions/ExpressionExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using EphemeralEx.Extensions;
+
+using NUnit.Framework;
+using FluentAssertions;
+
+
+namespace EphemeralExTests.Extensions
+{
+    class ExpressionExtensionsTests
+    {
+        [Test]
+        public void Do_WithAction_CallsActionAndReturnsSameInstance()
+        {
+            var obj = new TestInlineActionClass();
+
+            var result = obj.Do(o => o.Act());
+
+            obj.ActedOn.Should().BeTrue();
+            result.Should().BeSameAs(obj);
+        }
+
+        private class TestInlineActionClass
+        {
+            public bool ActedOn { get; set; } = false;
+
+            public void Act()
+            {
+                ActedOn = true;
+            }
+        }
+    }
+}
