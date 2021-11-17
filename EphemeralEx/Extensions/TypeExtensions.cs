31067da094848195852983557dd174d7e726d8c5
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-11-17 23:03:55 +1000

    Added Inherits

--- EphemeralEx/Extensions/TypeExtensions.cs
+++ EphemeralEx/Extensions/TypeExtensions.cs
@@ -14,5 +14,8 @@ namespace EphemeralEx.Extensions
 
         public static bool IsAttributed<T>(this Type type) where T : Attribute
             => type.IsDefined(typeof(T), false);
+
+        public static bool Inherits<T>(this Type type)
+            => typeof(T).IsAssignableFrom(type);
     }
 }
---
+++ EphemeralExTests/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using EphemeralEx.Extensions;
+
+using FluentAssertions;
+using NUnit.Framework;
+
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace EphemeralExTests.Extensions
+{
+    class TypeExtensionsTests
+    {
+        [Test]
+        public void Inherits_WithUnrelatedType_ReturnsFalse()
+        {
+            var result = typeof(Unrelated).Inherits<IBase>();
+
+            result.Should().Be(false);
+        }
+
+        [Test]
+        public void Inherits_WithImplementation_ReturnsTrue()
+        {
+            var result = typeof(Implementation).Inherits<IBase>();
+
+            result.Should().Be(true);
+
+        }
+
+        [Test]
+        public void Inherits_WithSubType_ReturnsTrue()
+        {
+            var result = typeof(Extensions).Inherits<Implementation>();
+
+            result.Should().Be(true);
+
+        }
+
+        private interface IBase {};
+        private class Implementation : IBase {};
+        private class Extensions : Implementation { };
+        private class Unrelated { };
+
+    }
+}
