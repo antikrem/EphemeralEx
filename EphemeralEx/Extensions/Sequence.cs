ca6fc3f1b77d22c1f45914fb1ed7b7044d3a96d1
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-12-14 00:23:57 +1000

    Added sequence

---
+++ EphemeralEx/Extensions/Sequence.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EphemeralEx.Extensions
+{
+    public static class Sequence
+    {
+        public static IEnumerable<T> From<T>(params IEnumerable<T>[] sequences)
+            => sequences.SelectMany(sequence => sequence);
+    }
+}
---
+++ EphemeralExTests/Extensions/SequenceTests.cs
@@ -0,0 +1,29 @@
+﻿using EphemeralEx.Extensions;
+
+using NUnit.Framework;
+using FluentAssertions;
+
+using System.Linq;
+using System.Collections.Generic;
+
+using EphemeralEx.Tests;
+
+
+namespace EphemeralExTests.Extensions
+{
+    class SequenceTests
+    {
+        [Test]
+        public void Sequence_From_ComposesEnumerables()
+        {
+            var sequence1 = new List<int> { Dummy.Int(), Dummy.Int() };
+            var sequence2 = new List<int> { Dummy.Int(), Dummy.Int(), Dummy.Int() };
+            var sequence3 = new List<int> { Dummy.Int(), Dummy.Int(), Dummy.Int(), Dummy.Int() };
+
+            var result = Sequence.From(sequence1, sequence2, sequence3);
+
+            result.Should().HaveCount(9);
+            result.Should().BeEquivalentTo(sequence1.Concat(sequence2).Concat(sequence3));
+        }
+    }
+}
