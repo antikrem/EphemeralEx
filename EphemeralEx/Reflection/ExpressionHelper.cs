11f5ae04c3b3be8dcd3efc3d8a3d5ebb8e4148c6
Chalinda Rodrigo <nobleservant49@gmail.com>
2022-09-06 23:48:27 +1000

    Added expression helper

---
+++ EphemeralEx/Reflection/ExpressionHelper.cs
@@ -0,0 +1,24 @@
+﻿using EphemeralEx.Extensions;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+
+namespace EphemeralEx.Reflection
+{
+    public static class ExpressionHelper
+    {
+        public static PropertyInfo SelectProperty<T>(Expression<Func<T, object>> select)
+            => typeof(T).GetProperty(GetMemberName(select)) ?? throw new ExpressionHelperException();
+
+        private static string GetMemberName<T>(Expression<Func<T, object>> selector)
+            => (selector.Body as MemberExpression)?.Member.Name ?? throw new ExpressionHelperException();
+    }
+
+    public class ExpressionHelperException : Exception
+    {
+        public ExpressionHelperException()
+            : base("Invalid selection function")
+        { }
+    }
+}
---
+++ EphemeralExTests/Reflection/ExpressionHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using EphemeralEx.Reflection;
+
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace EphemeralExTests.Reflection
+{
+    class ExpressionHelperTests
+    {
+        [Test]
+        public void SelectProperty_WithCorrectExpression_ReturnsCorrectReflectedInfo()
+        {
+            var result = ExpressionHelper.SelectProperty<ExampleClass>(example => example.Foo);
+
+            result.Should().Match(property => property.Name == "Foo");
+        }
+
+        [Test]
+        public void SelectProperty_WithIncorrectExpression_Throws()
+        {
+            Assert.Throws<ExpressionHelperException>(
+                () => ExpressionHelper.SelectProperty<ExampleClass>(example => 12)
+            );
+
+        }
+
+        private record ExampleClass(string Foo, object Bar, int Baz);
+    }
+}
